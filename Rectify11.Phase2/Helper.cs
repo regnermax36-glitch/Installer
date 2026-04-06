5f8e1ae575473659c3711fe2b4bd72fa0cc47dc2
regnermax36-glitch <regnermax36@gmail.com>
2026-04-06 15:40:43 +0200

    Add files via upload

--- Rectify11.Phase2/Helper.cs
+++ Rectify11.Phase2/Helper.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using System;
 using System.Diagnostics;
 using System.Globalization;
--- Rectify11.Phase2/Patches.cs
+++ Rectify11.Phase2/Patches.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.CodeDom.Compiler;
 using System.ComponentModel;
 using System.Diagnostics;
